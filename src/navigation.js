6e2536a406e744b1c0d4106d8b015964f1861ddf
asd1495 <armando.salcedo95@gmail.com>
2022-07-15 20:06:01 -0500

    migrated api fetch to axios and added navigation js

---
+++ src/navigation.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+    console.log({location});
+
+    if(location.hash.startsWith('#trends')) {
+        console.log('TRENDS')
+    } else if (location.hash.startsWith('#search=')) {
+        console.log('Search')
+    } else if (location.hash.startsWith('#movie=')) {
+        console.log('Movie')
+    } else if (location.hash.startsWith('#category=')) {
+        console.log('Categorie')
+    } else {
+        console.log('Home')
+    }
+    location.hash
+}
