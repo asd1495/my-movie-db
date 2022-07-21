19d1ec49058ebb538a9a68de989d9816a746c380
asd1495 <armando.salcedo95@gmail.com>
2022-07-20 21:32:13 -0500

    added a disposable api key for the website to work

---
+++ src/api.js
@@ -0,0 +1,3 @@
+//This is a disposable API only for the website to work on GH Pages, therefore I highly recommend to get your own by creating an account on https://www.themoviedb.org/ and add it to a hidden.js => .gitignore file
+
+const API_KEY = '25050df2802c2a4fa36d5afa51c88f98';
